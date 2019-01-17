12fe54b7730b562fc6004b632acf54af0323122c
Peter Kamb <peterkamb@gmail.com>
2019-01-17 10:03:49 -0800

    network controller class

---
+++ TableView/Controller/NetworkController.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkController.swift
+//  TableView
+//
+//  Created by Peter Kamb on 1/16/19.
+//  Copyright © 2019 Peter Kamb. All rights reserved.
+//
+
+import Foundation
+
+class NetworkController: NSObject, URLSessionDelegate {
+    
+    static let `default` = NetworkController()
+    
+    lazy var session: URLSession = {
+        return URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
+    }()
+    
+}
