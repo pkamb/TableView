ccb5aa31187c9876954705564db5d0ccfe218384
Peter Kamb <peterkamb@gmail.com>
2019-01-17 10:03:49 -0800

    add networknig

--- TableView/Model/Model.swift
+++ TableView/Model/Model.swift
@@ -25,3 +25,26 @@ struct LoremPicsum {
     }
     
 }
+
+extension LoremPicsum {
+    
+    struct Networking {
+        
+        enum URLs: String {
+            case getPhotos = "https://picsum.photos/list"
+        }
+        
+        func getPhotos(completion: @escaping ([Photo]?) -> ()) {
+            guard let url = URL(string: URLs.getPhotos.rawValue) else { return }
+            
+            let task = NetworkController.default.session.dataTask(with: url) { (data, response, error) in
+                guard let data = data else { completion(nil); return }
+                
+                let items = try? JSONDecoder().decode([Photo].self, from: data)
+                completion(items)
+            }
+            task.resume()
+        }
+    }
+    
+}
